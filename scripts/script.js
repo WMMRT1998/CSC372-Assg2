939208b4891e9aaa5b043342a0f7b6da9992c752
Russell <181926470+WMMRT1998@users.noreply.github.com>
2026-09-21 13:19:10 -0400

    saving now adds event info to <ul> in summary

--- scripts/script.js
+++ scripts/script.js
@@ -35,10 +35,29 @@ let summaryCounter = 0; //this is a flag that i will use to check how many event
 const cards = document.querySelectorAll('.event-card');
 for (const card of cards){
 
+    /*
+        This part of the code was made cause i wanted to get the data from each card in this loop
+        but i had never made id's or classes for each part. However, i always used h3 for event name
+        and 1st p was date/time, and 2nd p was always location.
+        So this section creates event details, which stores all the tags, i can then create a variable
+        for 1st and 2nd p element, and one for h3 title element, and finally can create a list item with
+        all this information combined. That list item can then be added and removed easily. Because this
+        is in the for loop, it is created for each event card, so i only have to do code once    
+    */ 
+    const eventDetails = card.querySelectorAll('p');
+    let eventName = card.querySelector('h3').innerText; 
+    let eventDate = eventDetails[0].innerText;
+    let eventLocation = eventDetails[1].innerText;
+
+    const eventItem = document.createElement('li');
+    eventItem.textContent = eventName + ", " + eventDate + ", " + eventLocation;
+    
     const button = document.createElement('button');
     button.style.width = '100px';
     button.style.height = '30px';
     button.textContent = 'Save This';
+
+    
     
     card.appendChild(button);
     button.addEventListener('click', onClick)
@@ -50,7 +69,7 @@ for (const card of cards){
         button.textContent = 'Remove This';
         flag = 1
         summaryCounter++;
-
+        list1.appendChild(eventItem);
         summary.removeChild(noSaved); //if something is added, this should always be removed
 
     }
@@ -61,6 +80,8 @@ for (const card of cards){
         flag = 0
         button.textContent = 'Save This';
         summaryCounter--;
+        
+        list1.removeChild(eventItem);
 
         //This check is to see how many events are saves, if none, it brings back no saved text
         if (summaryCounter === 0){
